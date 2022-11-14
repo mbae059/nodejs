54e695bb2be0e2410af8e467e8fd6e50ceba2eaf
Minjun Bae <mbae059@daum.net>
2022-11-14 13:31:11 +0900

    SAVE

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function templateList(data) {
   return fileList;
 }
 
-function templateHTML(title, fileList, body) {
+function templateHTML(title, fileList, control, body) {
   return `<!doctype html>
   <html>
   <head>
@@ -22,7 +22,7 @@ function templateHTML(title, fileList, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${fileList}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html> 
@@ -42,12 +42,13 @@ const app = http.createServer(function (request, response) {
         const template = templateHTML(
           title,
           fileList,
+          '<a href="/create">create</a>',
           `<h2>${title}</h2>${description}`
         );
         response.writeHead(200);
         response.end(template);
       });
-    } else {
+    } else if (queryData.id) {
       //queryString
       fs.readFile(`content/${queryData.id}`, "utf8", (err, description) => {
         if (err) throw err;
@@ -58,6 +59,13 @@ const app = http.createServer(function (request, response) {
           let template = templateHTML(
             title,
             fileList,
+            `<a href="/create">create</a> 
+            <a href="/update?id=${title}">update</a>
+            <form action="delete_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
+            </form>
+            `,
             `<h2>${title}</h2>${description}`
           );
 
@@ -74,7 +82,8 @@ const app = http.createServer(function (request, response) {
       const template = templateHTML(
         title,
         fileList,
-        `<form action="http://localhost:${portNumber}/create_process" method="post">
+        "",
+        `<form action="/create_process" method="post">
         <p><input type = "text" name ="title" placeholder="title"></p>
         <p>
           <textarea name = "description" placeholder="description"></textarea>
@@ -100,12 +109,77 @@ const app = http.createServer(function (request, response) {
       const description = post.description;
       fs.writeFile(`content/${title}`, description, "utf8", (err) => {
         if (err) throw err;
-        response.writeHead(200);
-        response.end("Success");
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
       });
     });
-    response.writeHead(200);
-    response.end("Success");
+  } else if (queryPathName === "/update") {
+    fs.readFile(`content/${queryData.id}`, "utf8", (err, description) => {
+      if (err) throw err;
+      fs.readdir("content", "utf-8", (err, data) => {
+        if (err) throw err;
+        const title = queryData.id;
+        const fileList = templateList(data);
+        let template = templateHTML(
+          title,
+          fileList,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+          `
+          <form action="/update_process" method="post">
+            <input type = "hidden" name="id" value = "${title}">
+            <p><input type = "text" name ="title" placeholder="title" value="${title}"></p>
+            <p>
+              <textarea name = "description" placeholder="description">${description}</textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `
+        );
+
+        response.writeHead(200);
+        response.end(template);
+      });
+    });
+  } else if (queryPathName === "/update_process") {
+    let body = "";
+    request.on("data", (data) => {
+      body += data;
+      console.log(data);
+    });
+    request.on("end", () => {
+      const post = qs.parse(body);
+      const id = post.id;
+      const title = post.title;
+      const description = post.description;
+
+      fs.rename(`content/${id}`, `content/${title}`, (err) => {
+        if (err) throw err;
+        fs.writeFile(`content/${title}`, description, "utf8", (err) => {
+          if (err) throw err;
+          response.writeHead(302, { Location: `/?id=${title}` });
+          response.end();
+        });
+      });
+    });
+  } else if (queryPathName === "/delete_process") {
+    let body = "";
+    request.on("data", (data) => {
+      body += data;
+      console.log(data);
+    });
+    request.on("end", () => {
+      const post = qs.parse(body);
+      const id = post.id;
+
+      fs.unlink(`content/${id}`, (err) => {
+        if (err) throw err;
+        console.log(`${id} is deleted`);
+        response.writeHead(302, { Location: `/` });
+        response.end();
+      });
+    });
   } else {
     response.writeHead(404);
     response.end("NOT FOUND");
